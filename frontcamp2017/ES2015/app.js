202ceb94e7c107bac18008e62a67ac06cae837bf
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2017-11-26 22:59:52 +0300

    Scrollup button

--- frontcamp2017/ES2015/app.js
+++ frontcamp2017/ES2015/app.js
@@ -1,6 +1,7 @@
 (function(){
     let navigationControlsElement = document.querySelector('.navigationControls');
     let backHomeButton = document.querySelector('#backHomeButton');
+    let scrollUpButton = document.querySelector('#scrollUpButton');
     let mainElement = document.querySelector('main');
 
     // Basic config for Articles and Channels components
@@ -12,6 +13,7 @@
         targetElement: mainElement,
         afterInserted() {
             navigationControlsElement.classList.remove('navigationControls--hidden');
+            backHomeButton.classList.remove('navigationControls-button--hidden');
         },
         step: 5
     };
@@ -23,6 +25,14 @@
     backHomeButton.addEventListener('click',  function() {
         document.dispatchEvent(new CustomEvent('showSourcesList'));
         navigationControlsElement.classList.add('navigationControls--hidden');
+    });
+
+    scrollUpButton.addEventListener('click',  function() {
+        window.scrollTo(0, 0);
+    });
+
+    document.addEventListener('scroll', () => {
+        window.pageYOffset > 500 ? scrollUpButton.classList.remove('navigationControls-button--hidden') : scrollUpButton.classList.add('navigationControls-button--hidden')
     });
 }());
 
