c1bf4c43228a0ddc6fe33eeff88a71141f906cc3
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2017-12-02 17:50:02 +0300

    Remove commented console.log babel plugin

--- frontcamp2017/ES2015/babel-plugin-remove-console-log/src/index.js
+++ frontcamp2017/ES2015/babel-plugin-remove-console-log/src/index.js
@@ -2,6 +2,15 @@ module.exports = function () {
     return {
         visitor: {
             ExpressionStatement(path) {
+                // Remove all commented console.log expressions
+                if (path.node.trailingComments && path.node.trailingComments.length) {
+                    if (path.node.trailingComments[0].value.indexOf('console.log') !== -1) {
+                        path.remove();
+                        return;
+                    }
+                }
+
+                // Remove all not-commented console.log expressions
                 if (!path.node.expression.callee) {
                     return;
                 }
