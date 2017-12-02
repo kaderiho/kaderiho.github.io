e4c7b63767682d0692626a95236a49610f418748
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2017-12-02 17:51:52 +0300

    Back app.js

--- frontcamp2017/ES2015/app.js
+++ frontcamp2017/ES2015/app.js
@@ -1,2 +1,36 @@
-console.log();
-//console.log();
+const navigationControlsElement = document.querySelector('.navigationControls');
+const backHomeButton = document.querySelector('#backHomeButton');
+const scrollUpButton = document.querySelector('#scrollUpButton');
+const mainElement = document.querySelector('main');
+
+// Basic config for Articles and Channels components
+const channelsConfig = {
+    targetElement: mainElement
+};
+
+const articlesConfig = {
+    targetElement: mainElement,
+    afterInserted() {
+        navigationControlsElement.classList.remove('navigationControls--hidden');
+        backHomeButton.classList.remove('navigationControls-button--hidden');
+    },
+    step: 5
+};
+
+// Initialize Articles and Channels list components
+new Channels(channelsConfig);
+new Articles(articlesConfig);
+
+backHomeButton.addEventListener('click', () => {
+    document.dispatchEvent(new CustomEvent('showChannelsList'));
+    navigationControlsElement.classList.add('navigationControls--hidden');
+});
+
+scrollUpButton.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+});
+document.addEventListener('scroll', () => {
+    window.pageYOffset > 500 ?
+        scrollUpButton.classList.remove('navigationControls-button--hidden') :
+        scrollUpButton.classList.add('navigationControls-button--hidden');
+});
