f95add9392d09d15cfb1946be47ce3e4c655f7f7
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2017-12-09 16:33:22 +0300

    Copy sources plugin

--- frontcamp2017/ES2015/webpack.config.prod.js
+++ frontcamp2017/ES2015/webpack.config.prod.js
@@ -1,9 +1,23 @@
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(baseConfig, {
     plugins: [
-        new UglifyJsPlugin()
+        new UglifyJsPlugin(),
+
+        new HtmlWebpackPlugin({
+            title: 'News feed',
+            template: 'index.html'
+        }),
+
+        new CopyWebpackPlugin([
+            {
+                from: 'data/',
+                to: '../dist/data'
+            }
+        ])
     ]
 });
