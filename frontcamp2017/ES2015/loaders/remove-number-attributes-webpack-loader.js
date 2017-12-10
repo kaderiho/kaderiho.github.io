cbd4407fa0cd4f0fdb527d927a0c76c51122e620
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2017-12-10 23:10:05 +0300

    Refctoring removing properties loader

--- frontcamp2017/ES2015/loaders/remove-number-attributes-webpack-loader.js
+++ frontcamp2017/ES2015/loaders/remove-number-attributes-webpack-loader.js
@@ -2,13 +2,15 @@ module.exports = function(source) {
     let parsedSourceJSON = JSON.parse(source);
 
     function loopThroughObjectProps(obj) {
-        for (var k in obj) {
-            if (typeof obj[k] == "object" && obj[k] !== null){
-                loopThroughObjectProps(obj[k]);
-            } else {
-                // Remove property if its a number
-                if (!isNaN(+k)) {
-                    delete obj[k];
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                if (typeof obj[prop] == "object" && obj[prop] !== null){
+                    loopThroughObjectProps(obj[prop]);
+                } else {
+                    // Remove property if its a number
+                    if (!isNaN(+prop)) {
+                        delete obj[prop];
+                    }
                 }
             }
         }
