d0f5a55f2ca89368462dbd43c80dc31f626d3664
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2017-12-19 11:18:22 +0300

    Store Class methods in prototype

--- frontcamp2017/ES2015/src/js/components/articles-list/articles-list.js
+++ frontcamp2017/ES2015/src/js/components/articles-list/articles-list.js
@@ -35,59 +35,59 @@ export default class Articles {
                     <p class="articleDescription">${articleItem.description ? articleItem.description : ''}</p>
                 </li>`;
     }
-
-    _subscribeOnEvents() {
-        document.addEventListener('showArticlesList', (e) => {
-            Articles.getArticles(e.detail.channelKey)
-                .then((articlesList) => {
-                    this.articlesList = articlesList.articles;
-                    this._lastItemIndex = 0;
-                    this.render(this.articlesList);
-                });
-        }, false);
-    }
-
-    _attachActionHandlers() {
-        this.showMoreElement.addEventListener('click', () => {
-            this.articlesListElement.innerHTML = this.articlesListElement.innerHTML + this._uploadNewArticles();
-
-            if (this._lastItemIndex === this.articlesList.length) {
-                this.showMoreElement.classList.add('articlesList-showMoreButton--hidden');
-            }
-        });
-    }
-
-    _uploadNewArticles() {
-        const limit = this._lastItemIndex + this._step;
-        let outputArticlesList = '';
-
-        for (let i = this._lastItemIndex; i < limit; i++) {
-            if (this.articlesList[i]) {
-                outputArticlesList += Articles.parseArticle(this.articlesList[i]);
-                this._lastItemIndex++;
-            } else {
-                break;
-            }
-        }
-
-        return outputArticlesList;
-    }
-
-    render() {
-        this.articlesListElement = document.createElement('ul');
-        this.articlesListElement.className = 'articlesList';
-        this.showMoreElement = document.createElement('button');
-        this.showMoreElement.className = 'btn articlesList-showMoreButton';
-        this.showMoreElement.innerText = 'Show More';
-
-        this.targetElement.innerHTML = '';
-        this.targetElement.appendChild(this.articlesListElement);
-        this.targetElement.appendChild(this.showMoreElement);
-
-        this.articlesListElement.innerHTML = this._uploadNewArticles();
-        this._afterInserted();
-
-        this._attachActionHandlers();
-        window.scrollTo(0, 0);
-    }
 }
+
+Articles.prototype.render = function() {
+    this.articlesListElement = document.createElement('ul');
+    this.articlesListElement.className = 'articlesList';
+    this.showMoreElement = document.createElement('button');
+    this.showMoreElement.className = 'btn articlesList-showMoreButton';
+    this.showMoreElement.innerText = 'Show More';
+
+    this.targetElement.innerHTML = '';
+    this.targetElement.appendChild(this.articlesListElement);
+    this.targetElement.appendChild(this.showMoreElement);
+
+    this.articlesListElement.innerHTML = this._uploadNewArticles();
+    this._afterInserted();
+
+    this._attachActionHandlers();
+    window.scrollTo(0, 0);
+};
+
+Articles.prototype._subscribeOnEvents = function() {
+    document.addEventListener('showArticlesList', (e) => {
+        Articles.getArticles(e.detail.channelKey)
+            .then((articlesList) => {
+                this.articlesList = articlesList.articles;
+                this._lastItemIndex = 0;
+                this.render(this.articlesList);
+            });
+    }, false);
+};
+
+Articles.prototype._attachActionHandlers = function() {
+    this.showMoreElement.addEventListener('click', () => {
+        this.articlesListElement.innerHTML = this.articlesListElement.innerHTML + this._uploadNewArticles();
+
+        if (this._lastItemIndex === this.articlesList.length) {
+            this.showMoreElement.classList.add('articlesList-showMoreButton--hidden');
+        }
+    });
+};
+
+Articles.prototype._uploadNewArticles = function() {
+    const limit = this._lastItemIndex + this._step;
+    let outputArticlesList = '';
+
+    for (let i = this._lastItemIndex; i < limit; i++) {
+        if (this.articlesList[i]) {
+            outputArticlesList += Articles.parseArticle(this.articlesList[i]);
+            this._lastItemIndex++;
+        } else {
+            break;
+        }
+    }
+
+    return outputArticlesList;
+};
--- frontcamp2017/ES2015/src/js/components/channels-list/channels-list.js
+++ frontcamp2017/ES2015/src/js/components/channels-list/channels-list.js
@@ -21,43 +21,43 @@ export default class Channels {
                  </li>`;
     }
 
-    _subscribeOnEvents() {
-        document.addEventListener('showChannelsList', () => {
-            this.render(this.targetElement, this.channels);
-        });
-    }
-
-    _attachActionHandlers() {
-        const channelsList = this.channesListElement.querySelectorAll('.channelsList-item');
-        let showArticlesEvent = new CustomEvent('showArticlesList', {detail: {}});
-
-        for (let channelItem of channelsList) {
-            channelItem.addEventListener('click', () => {
-                showArticlesEvent.detail.channelKey = channelItem.getAttribute('data-key');
-                document.dispatchEvent(showArticlesEvent);
-            });
-        }
-    }
-
-    render(targetElement, channelsList) {
-        let channelsListOutput = '';
-
-        this.channesListElement = document.createElement('ul');
-        this.channesListElement.className = 'channelsList';
-
-        targetElement.innerHTML = '';
-        targetElement.appendChild(this.channesListElement);
-
-        for (let channel of channelsList) {
-            channelsListOutput += Channels.parseChannel(channel);
-        }
-
-        this.channesListElement.innerHTML = channelsListOutput;
-        this._attachActionHandlers();
-    }
-
     async init(targetElement) {
         this.channels = (await Channels.getChannels()).channels;
         this.render(targetElement, this.channels);
     }
 }
+
+Channels.prototype._subscribeOnEvents = function() {
+    document.addEventListener('showChannelsList', () => {
+        this.render(this.targetElement, this.channels);
+    });
+};
+
+Channels.prototype._attachActionHandlers = function() {
+    const channelsList = this.channesListElement.querySelectorAll('.channelsList-item');
+    let showArticlesEvent = new CustomEvent('showArticlesList', {detail: {}});
+
+    for (let channelItem of channelsList) {
+        channelItem.addEventListener('click', () => {
+            showArticlesEvent.detail.channelKey = channelItem.getAttribute('data-key');
+            document.dispatchEvent(showArticlesEvent);
+        });
+    }
+};
+
+Channels.prototype.render = function(targetElement, channelsList) {
+    let channelsListOutput = '';
+
+    this.channesListElement = document.createElement('ul');
+    this.channesListElement.className = 'channelsList';
+
+    targetElement.innerHTML = '';
+    targetElement.appendChild(this.channesListElement);
+
+    for (let channel of channelsList) {
+        channelsListOutput += Channels.parseChannel(channel);
+    }
+
+    this.channesListElement.innerHTML = channelsListOutput;
+    this._attachActionHandlers();
+};
