cf779f37ec4bab7b7eccd4642c5f5fccc7ba14ad
Konstantin Aderiho <k.aderiho@tut.by>
2018-01-30 16:22:10 +0300

    Fix Articles Model

---
+++ frontcamp2017/nodejs-expressjs-hometask/models/articleModel.js
@@ -0,0 +1,7 @@
+// Moch articles list (will be replaced on a Model)
+var articlesList = [{id: 0, title: 'blog title 1', description: 'blog description 1'},
+    {id: 1, title: 'blog title 2', description: 'blog description 2'},
+    {id: 2, title: 'blog title 3', description: 'blog description 3'},
+    {id: 3, title: 'blog title 4', description: 'blog description 4'}];
+
+module.exports = articlesList;
