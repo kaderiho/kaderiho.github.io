f1922f6bf650669a91af5d7903adb531107741d3
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2018-02-02 19:54:22 +0300

    User log out mechanism has been added

--- frontcamp2017/nodejs-expressjs-hometask/routes/index.js
+++ frontcamp2017/nodejs-expressjs-hometask/routes/index.js
@@ -2,10 +2,8 @@ const router    = require('express').Router();
 const passport  = require('passport');
 
 router.get('/', (req, res) => {
-    res.format({
-        'text/html': () => res.render('index', {
-            user: req.user
-        })
+    res.render('index', {
+        user: req.user
     });
 });
 
