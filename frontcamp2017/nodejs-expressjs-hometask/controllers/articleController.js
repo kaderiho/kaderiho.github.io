57637476ae293b16dae55f9b26d8da97283b086f
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2018-02-10 17:03:44 +0300

    Replace callbacks on then in Article model

--- frontcamp2017/nodejs-expressjs-hometask/controllers/articleController.js
+++ frontcamp2017/nodejs-expressjs-hometask/controllers/articleController.js
@@ -1,54 +1,50 @@
 const Article = require('../models/ArticleModel');
 
 const getArticles = (req, res, next) => {
-    Article.find({}, (err, data) => {
-        if (err) {
+    Article.find({})
+        .then((data) => {
+            res.send(JSON.stringify(data));
+        }, (err) => {
             next(err);
-        }
-        res.send(JSON.stringify(data));
-    });
+        })
 };
 
 const getArticle = (req, res, next) => {
-    Article.find({id: +req.params.id}, (err, data) => {
-        if (err) {
+    Article.find({ id: +req.params.id })
+        .then((data) => {
+            res.send(data);
+        }, (err) => {
             next(err);
-        }
-        res.send(data);
-    });
+        });
 };
 
 const updateArticle = (req, res, next) => {
     Article.findByIdAndUpdate(
         { id: +req.params.id },
-        { description: req.body.description, title: req.body.title },
-        (err, data) => {
-            if (err) {
-                next(err);
-            }
+        { description: req.body.description, title: req.body.title })
+        .then((data) => {
             res.send(data);
-    });
+        }, (err) => {
+            next(err);
+        });
 };
 
 const createArticle = (req, res) => {
-    Article.create({
-        title: req.body.title,
-        description: req.body.description
-    }, (err, data) => {
-        if (err) {
+    Article.create({ title: req.body.title, description: req.body.description })
+        .then((data) => {
+            res.send(data);
+        }, (err) => {
             next(err);
-        }
-        res.send(data);
-    });
+        })
 };
 
 const deleteArticle = (req, res, next) => {
-    Article.findByIdAndRemove(req.params.id, (err, data) => {
-        if (err) {
+    Article.findByIdAndRemove(req.params.id)
+        .then((data) => {
+            res.send(data);
+        }, (err) => {
             next(err);
-        }
-        res.send(data);
-    });
+        })
 };
 
 module.exports.deleteArticle = deleteArticle;
