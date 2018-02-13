ba4a2ffa627c17244c6ac624c5079a458c05ba91
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2018-02-13 12:42:15 +0300

    Fix callbacks for article CRUD methods

--- frontcamp2017/nodejs-expressjs-hometask/controllers/articleController.js
+++ frontcamp2017/nodejs-expressjs-hometask/controllers/articleController.js
@@ -2,18 +2,14 @@ const Article = require('../models/ArticleModel');
 
 const getArticles = (req, res, next) => {
     Article.find({})
-        .then((data) => {
-            res.send(JSON.stringify(data));
-        }, (err) => {
+        .then((data) => res.json(data), (err) => {
             next(err);
         })
 };
 
 const getArticle = (req, res, next) => {
     Article.find({ id: +req.params.id })
-        .then((data) => {
-            res.send(data);
-        }, (err) => {
+        .then((data) => res.json(data), (err) => {
             next(err);
         });
 };
@@ -22,27 +18,21 @@ const updateArticle = (req, res, next) => {
     Article.findByIdAndUpdate(
         { id: +req.params.id },
         { description: req.body.description, title: req.body.title })
-        .then((data) => {
-            res.send(data);
-        }, (err) => {
+        .then((data) => res.json(data), (err) => {
             next(err);
         });
 };
 
 const createArticle = (req, res) => {
     Article.create({ title: req.body.title, description: req.body.description })
-        .then((data) => {
-            res.send(data);
-        }, (err) => {
+        .then((data) => res.json(data), (err) => {
             next(err);
         })
 };
 
 const deleteArticle = (req, res, next) => {
     Article.findByIdAndRemove(req.params.id)
-        .then((data) => {
-            res.send(data);
-        }, (err) => {
+        .then((data) => res.json(data), (err) => {
             next(err);
         })
 };
