515ea9e9aef6ef9a20da49f71f92e860dba48d95
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2018-03-09 21:40:52 +0300

    Redux: blog filtering

--- frontcamp2017/reactjs-hometask/src/client/components/blogs-list/blogs-list.jsx
+++ frontcamp2017/reactjs-hometask/src/client/components/blogs-list/blogs-list.jsx
@@ -11,7 +11,7 @@ class BlogsList extends React.Component {
     render() {
         return(
             <div className="blogsList">
-                {this.props.map((blogItem) => <BlogItem key={blogItem.id} blog={blogItem}/>)}
+                {this.props.blogs.map((blogItem) => <BlogItem key={blogItem.id} blog={blogItem}/>)}
             </div>
         )
     }
