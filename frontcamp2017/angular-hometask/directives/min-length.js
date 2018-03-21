1b9e8ba442cb5cc6910b2a7da1050a2797c85bb2
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2018-03-21 22:51:34 +0300

    2-3 tasks

---
+++ frontcamp2017/angular-hometask/directives/min-length.js
@@ -0,0 +1,20 @@
+app.directive('kaMinLength', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            var min = parseInt(attrs.minLength || 0, 10);
+
+            ctrl.$parsers.unshift(function(viewValue) {
+                var len = viewValue ? viewValue.length - (viewValue.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g)||[]).length : 0;
+                if (len <= min) {
+                    ctrl.$setValidity("minlength", false);
+                    return undefined;
+                } else {
+                    ctrl.$setValidity("minlength", true);
+                    return viewValue;
+                }
+            });
+        }
+    };
+});
