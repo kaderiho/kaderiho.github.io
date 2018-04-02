631bb0870c8adf82b5e36b8789e35c7afcc0baeb
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2018-04-02 16:58:50 +0300

    UI tests for react

--- frontcamp2017/reactjs-hometask/tests/components/article-adding.test.js
+++ frontcamp2017/reactjs-hometask/tests/components/article-adding.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Enzyme from 'enzyme';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { spy } from 'sinon';
 import Adapter from 'enzyme-adapter-react-16';
 import ArticleAdding from '../../src/browser/components/article/article-adding';
 import configureStore from 'redux-mock-store';
@@ -9,14 +10,38 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('<ArticleAdding />', () => {
     const mockStore = configureStore();
-    let store, container;
+    const initialState = {};
+    let store, componentContainer;
 
-    beforeEach(()=>{
+    beforeEach(() => {
         store = mockStore(initialState);
-        container = shallow(<ArticleAdding store={store} /> )
+        componentContainer = mount(<ArticleAdding store={store}/> );
     });
 
+    // Markup testing
     it('Rendered a component', () => {
-        expect(container.length).toEqual(1);
+        expect(componentContainer.length).toEqual(1);
+    });
+
+    it('There is a form', () => {
+        expect(componentContainer.find('.addingTodoForm').length).toEqual(1);
+    });
+
+    it('There are 2 inputs inside the form', () => {
+        expect(componentContainer.find('input').length).toEqual(2);
+    });
+
+    it('There is a submit button inside the form', () => {
+        expect(componentContainer.find('button').length).toEqual(1);
+    });
+
+    // UI actions testing
+    it('simulates click events', () => {
+        // const callback = spy();
+        // const wrapper = mount(<ArticleAdding store={store} onSubmit={callback}/> );
+        //
+        // componentContainer.find('form').simulate('submit');
+        // expect(callback).to.have.been.called();
+        // expect(onButtonClick).to.have.property('callCount', 1);
     });
 });
