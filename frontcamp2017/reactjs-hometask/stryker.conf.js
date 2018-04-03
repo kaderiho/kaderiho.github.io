5a90179bac98261f372d0f75436a316cb24fe8a3
Kanstantsin Adzerykha <Kanstantsin_Adzerykha@epam.com>
2018-04-03 18:08:41 +0300

    Initiate stryker-mutator

---
+++ frontcamp2017/reactjs-hometask/stryker.conf.js
@@ -0,0 +1,20 @@
+module.exports = function(config) {
+  config.set({
+    files: [
+      {
+        pattern: "src/**/*.js",
+        mutated: true,
+        included: false
+      },
+      "tests/**/*.js"
+    ],
+    testRunner: "jest",
+    mutator: "javascript",
+    babelrcFile: '.babelrc', // Location of your .babelrc file
+    transpilers: [
+      'babel' // Specify that your code needs to be transpiled before tests can be run
+    ],
+    reporter: ["clear-text", "progress"],
+    coverageAnalysis: "off"
+  });
+};
